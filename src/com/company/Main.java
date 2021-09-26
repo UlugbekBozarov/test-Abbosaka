f1b0ace766ca22366b41dd6b283cae77a57f8a17
UlugbekBozarov <ulugbek01081999@gmail.com>
2021-09-26 16:07:31 +0500

    new test project

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import com.company.domain.Fish;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello evryone!");
+
+        List<Fish> fishList = new ArrayList<>();
+
+        int malefish = 10;
+        int femalefish = 10;
+
+    }
+}
